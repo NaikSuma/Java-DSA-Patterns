53d19d0f9aa85f1c93de414a7b96c43c2e8f5436
NaikSuma <109804241+NaikSuma@users.noreply.github.com>
2026-07-03 20:43:46 +0530

    Update 367. Valid Perfect Square_Brute.java

--- Binary_Search/Basic/367. Valid Perfect Square_Brute.java
+++ Binary_Search/Basic/367. Valid Perfect Square_Brute.java
@@ -1,1 +1,19 @@
 
+class Solution {
+    public boolean isPerfectSquare(int num) {
+        
+        for(int i=1;i<=num;i++){
+            long square=(long)i*i;
+            if(square==num){
+                return true;
+            }
+            if(square>num){
+                break;
+            }
+        }
+        return false;
+
+       
+        return false;
+    }
+}
