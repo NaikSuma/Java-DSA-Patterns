e31587d733b4b3edd011a65bef0f6b0c4fe1329e
NaikSuma <109804241+NaikSuma@users.noreply.github.com>
2026-07-03 20:38:07 +0530

    Update 35.Search_Insert_Pos.optimal.java

--- Binary_Search/Basic/35.Search_Insert_Pos.optimal.java
+++ Binary_Search/Basic/35.Search_Insert_Pos.optimal.java
@@ -1,1 +1,38 @@
 
+class Solution {
+    public int searchInsert(int[] nums, int target) {
+        
+        /
+
+
+        //1.Brute 
+        // for(int i=0;i<nums.length;i++){
+        //     if(nums[i]>=target){
+        //         return i;
+        //     }
+        // }
+        // return nums.length;
+
+        //2.Optimal->binary
+
+        int n=nums.length;
+        int low=0;
+        int high=n-1;
+
+        while(low<=high){
+            int mid=low+(high-low)/2;
+
+            if(nums[mid]==target){
+                return mid;
+            }
+            else if(nums[mid]<target){
+                low=mid+1;
+            }
+            else{
+                high=mid-1;
+            }
+        }
+
+    return low;
+    }
+}
