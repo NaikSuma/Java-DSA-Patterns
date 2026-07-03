79f9d0d5be052280d85e3180eea191c76e3d36fa
NaikSuma <109804241+NaikSuma@users.noreply.github.com>
2026-07-03 20:55:01 +0530

    Update 33. Search in Rotated Sorted Array_Optimal.java

--- Binary_Search/Rotated_Array/ss.java
+++ Binary_Search/Rotated_Array/ss.java
@@ -1,1 +1,49 @@
 
+class Solution {
+    public int search(int[] nums, int target) {
+       
+
+        //2.Optimal ->binary search
+
+        int low=0;
+        int  high=nums.length-1;
+
+        while(low<=high){
+            int mid=low+(high-low)/2;
+
+
+            if(nums[mid]==target){
+                return mid;
+            }
+
+            //left part is sorted
+            if(nums[low]<=nums[mid]){
+
+                //target lies in a sorted left
+                if(nums[low]<=target&&target<nums[mid]){
+                     high=mid-1;
+            }
+            else{
+
+                //else search in a right half
+                low=mid+1;
+            }
+            }
+            else{
+                //right part is sorted
+
+                if(nums[mid]<target&&target<=nums[high]){
+                    //target lies in a right part
+                    low=mid+1;
+                }
+                else{
+                    high=mid-1;
+                }
+            }
+        }
+
+        return -1;
+
+
+    }
+}
