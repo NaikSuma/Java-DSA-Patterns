6eb22c4d6bde9a83d7f0cf1e3c4730afd0561181
NaikSuma <109804241+NaikSuma@users.noreply.github.com>
2026-07-03 17:55:14 +0530

    Update 1011_Capacity_To_Ship_WithInDDays_Brute.java
    BruteForce

--- Binary_Search/1011_Capacity_To_Ship_WithInDDays_Brute.java
+++ Binary_Search/1011_Capacity_To_Ship_WithInDDays_Brute.java
@@ -1,1 +1,39 @@
 
+class Solution {
+
+    public int shipWithinDays(int[] weights, int days) {
+
+       int low=0;
+       int high=0;
+
+       //calculating low and high
+       for(int weight:weights){
+            low=Math.max(weight,low);
+            high+=weight;
+       }
+
+       //capacity
+       for(int capacity=low;capacity<=high;capacity++){
+            if(canShip(weights,days,capacity)){
+                return capacity;
+            }
+       }
+
+       return high;
+    }
+
+    private boolean canShip(int weights[],int days,int capacity){
+        int currentWeight=0;
+        int daysCount=1;
+
+        for(int weight:weights){
+            currentWeight+=weight;
+            if(currentWeight>capacity){
+                daysCount++;
+                currentWeight=weight;
+        }
+        }
+
+        return daysCount<=days;
+    }
+}
