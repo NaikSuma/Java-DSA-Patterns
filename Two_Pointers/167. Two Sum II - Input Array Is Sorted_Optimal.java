44a146dfd858e366545b42d142f7f9a4e62d332a
NaikSuma <109804241+NaikSuma@users.noreply.github.com>
2026-07-04 23:15:19 +0530

    Update 167. Two Sum II - Input Array Is Sorted_Optimal.java

--- Two_Pointers/167. Two Sum II - Input Array Is Sorted_Optimal.java
+++ Two_Pointers/167. Two Sum II - Input Array Is Sorted_Optimal.java
@@ -1,1 +1,25 @@
 
+class Solution {
+    public int[] twoSum(int[] numbers, int target) {
+
+        //2. optimised
+        int left=0;
+        int right=numbers.length-1;
+        int sum=0;
+
+        while(left<=right){
+            sum=numbers[left]+numbers[right];
+            if(target==sum){
+                return new int[]{left+1,right+1};
+            }
+            else if(sum<target){
+                left++;
+            }else{
+                right--;
+            }
+        }
+        return new int[]{};
+
+
+    }
+}
