c75494915f8cd658fdf3a6a01ba30b94a8a13e53
NaikSuma <109804241+NaikSuma@users.noreply.github.com>
2026-07-04 23:19:31 +0530

    Update 3sum_Brute.java

--- Two_Pointers/3sum.java
+++ Two_Pointers/3sum.java
@@ -1,28 +1,6 @@
 
 class Solution {
-    // public List<List<Integer>> threeSum(int[] nums) {
-    //     Set<List<Integer>> ans=new HashSet<>();
-    //     int n=nums.length;
-    //     for(int i=0;i<n;i++){
-    //         Set<Integer> hashSet=new HashSet<>();
-    //         for(int j=i+1;j<n;j++){
-    //             //for(int k=j+1;k<n;k++){
-    //                 int third=-(nums[i]+nums[j]);
-
-
-    //                 if(hashSet.contains(third)){
-    //                     List<Integer> temp=Arrays.asList(nums[i],nums[j],third);
-    //                     Collections.sort(temp);
-    //                     ans.add(temp);
-
-    //                 }
-    //                 hashSet.add(nums[j]);
-
-    //         }
-
-    //     }
-    //     return new ArrayList<>(ans);
-    // }
+ 
    
 
    //1. Brute force
