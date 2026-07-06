b130ca82f94a712553e43a16d70e5243eccb289f
NaikSuma <109804241+NaikSuma@users.noreply.github.com>
2026-07-06 22:48:02 +0530

    Update 4sum_Brute.java

--- Two_Pointers/4sum_Brute.java
+++ Two_Pointers/4sum_Brute.java
@@ -1,1 +1,29 @@
 
+import java.util.*;
+
+class Solution {
+   
+    //1.Bruteforce
+
+    public List<List<Integer>> fourSum(int[] nums, int target){
+        Set<List<Integer>> st=new HashSet<>();
+        int n=nums.length;
+
+        for(int i=0;i<n;i++){
+            for(int j=i+1;j<n;j++){
+                for(int k=j+1;k<n;k++){
+                    for(int l=k+1;l<n;l++){
+                        int sum=nums[i]+nums[j]+nums[k]+nums[l];
+                        if(sum==target){
+                            List<Integer> temp=Arrays.asList(nums[i],nums[j],nums[k],nums[l]);
+                            Collections.sort(temp);
+                            st.add(temp);
+                        }
+                    }
+                }
+            }
+        }
+        return new ArrayList<>(st);
+    } 
+    
+}
