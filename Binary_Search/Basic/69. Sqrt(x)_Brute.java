1aefa6d781eee7ec1dfc25f435588966e31cac50
NaikSuma <109804241+NaikSuma@users.noreply.github.com>
2026-08-06 16:32:19 +0530

    Update 69. Sqrt(x)_Brute.java

--- Binary_Search/Basic/69. Sqrt(x)_Brute.java
+++ Binary_Search/Basic/69. Sqrt(x)_Brute.java
@@ -1,7 +1,7 @@
 class Solution {
     public int mySqrt(int x) {
 //1.Brute
-       int ans=0;
+       int ans=0; 
        for(int i=1;i<=x;i++){
             if((long)i*i<=x){
                 ans=i;
