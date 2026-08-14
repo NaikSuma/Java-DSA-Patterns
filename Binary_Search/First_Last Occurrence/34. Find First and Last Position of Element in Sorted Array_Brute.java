b1d2c04de1c071b702373cb0772fd03be79126e2
Suma <109804241+NaikSuma@users.noreply.github.com>
2026-08-14 21:43:13 +0530

    Update 34. Find First and Last Position of Element in Sorted Array_Brute.java

--- Binary_Search/First_Last Occurrence/34. Find First and Last Position of Element in Sorted Array_Brute.java
+++ Binary_Search/First_Last Occurrence/34. Find First and Last Position of Element in Sorted Array_Brute.java
@@ -6,7 +6,7 @@ class Solution {
         int last=-1;
 
         for(int i=0;i<nums.length;i++){
-            if(nums[i]==target){
+            if(nums[i]==target){ 
                 if(first==-1){
                     first=i;
                 }
