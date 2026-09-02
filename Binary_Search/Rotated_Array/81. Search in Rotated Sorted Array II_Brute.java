07cf95e0e26ede16ae3e5b3630d85ede521ab543
Suma <109804241+NaikSuma@users.noreply.github.com>
2026-09-02 21:58:40 +0530

    Update 81. Search in Rotated Sorted Array II_Brute.java

--- Binary_Search/Rotated_Array/81. Search in Rotated Sorted Array II_Brute.java
+++ Binary_Search/Rotated_Array/81. Search in Rotated Sorted Array II_Brute.java
@@ -1,7 +1,7 @@
 class Solution {
     public boolean search(int[] nums, int target) {
         
-        // 1.bruteforce 
+        // 1.bruteforce  
 
         for(int i=0;i<nums.length;i++){
             if(nums[i]==target){ 
